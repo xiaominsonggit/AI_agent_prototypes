2895eab6748fadccacc713079f8dc140482b7884
Xiaomin Song <xiaomin.song.git@gmail.com>
2024-11-06 11:07:08 -0800

    add ReAct agent example

---
+++ AI_agents/ReAct/ReAct.py
@@ -0,0 +1,78 @@
+import openai
+import re
+import httpx
+import os
+from dotenv import load_dotenv
+
+_ = load_dotenv()
+from openai import OpenAI
+
+
+class Agent():
+    def __init__(self, system=""):
+        self.client = OpenAI(
+            api_key = os.environ.get("OPENAI_API_KEY"),
+            )
+        self.system = system
+        self.messages = []
+        if self.system:
+            self.messages.append({"role": "system", "content": system})
+
+    def __call__(self, message):
+        self.messages.append({"role": "user", "content": message})
+        result = self.execute()
+        self.messages.append({"role": "assistant", "content": result})
+        return result
+
+    def execute(self):
+        completion = self.client.chat.completions.create(
+                        model="gpt-4o-mini", 
+                        temperature=0,
+                        messages=self.messages)
+        return completion.choices[0].message.content
+
+def calculate(what):
+    return eval(what)
+
+def average_dog_weight(name):
+    if name in "Scottish Terrier": 
+        return("Scottish Terriers average 20 lbs")
+    elif name in "Border Collie":
+        return("a Border Collies average weight is 37 lbs")
+    elif name in "Toy Poodle":
+        return("a toy poodles average weight is 7 lbs")
+    else:
+        return("An average dog weights 50 lbs")
+
+known_actions = {
+    "calculate": calculate,
+    "average_dog_weight": average_dog_weight
+}
+
+# python regular expression to selection action
+action_re = re.compile('^Action: (\w+): (.*)$')
+
+def query(question, prompt, max_turns=5):
+    i = 0
+    bot = Agent(prompt)
+    next_prompt = question
+    while i < max_turns:
+        i += 1
+        result = bot(next_prompt)
+        print(result)
+        actions = [
+            action_re.match(a) 
+            for a in result.split('\n') 
+            if action_re.match(a)
+        ]
+        if actions:
+            # There is an action to run
+            action, action_input = actions[0].groups()
+            if action not in known_actions:
+                raise Exception("Unknown action: {}: {}".format(action, action_input))
+            print(" -- running {} {}".format(action, action_input))
+            observation = known_actions[action](action_input)
+            print("Observation:", observation)
+            next_prompt = "Observation: {}".format(observation)
+        else:
+            return
